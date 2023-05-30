fc9f59dd8f15a7c60df15bfb98f41119b1508ec7
lenakravhi <101953369+lenakravhi@users.noreply.github.com>
2023-05-30 03:07:53 +0300

    Update simple_todo_list.js

--- simple_todo_list.js
+++ simple_todo_list.js
@@ -1,103 +1,98 @@
 const todo = {
-  // Обработчик событий действий
+// Обробник подій дій
   action(e) {
     const target = e.target;
-    // Проверяем, является ли целевой элемент кнопкой действия
-    if (target.classList.contains('todo__action')) {
-      const action = target.dataset.todoAction;
-      const elemItem = target.closest('.todo__item');
-      // Проверяем тип действия и текущее состояние элемента
-      if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
-        // Если действие - "удалить" и элемент уже помечен как "удаленный", то удаляем элемент
-        elemItem.remove();
-      } else {
-        // Иначе, устанавливаем новое состояние элемента
-        elemItem.dataset.todoState = action;
-        // Создаем словарь, содержащий соответствие действия и его текстового описания
+// Перевіряємо, чи є цільовий елемент кнопкою дії
+if (target.classList.contains('todo__action')) {
+  const action = target.dataset.todoAction;
+  const elemItem = target.closest('.todo__item');
+  // Перевіряємо тип дії та поточний стан елемента
+  if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
+    // Якщо дію - "видалити" і елемент вже позначений як "видалений", то видаляємо елемент
+    elemItem.remove();
+  } else {
+    // Інакше встановлюємо новий стан елемента
+    elemItem.dataset.todoState = action;
+    // Створюємо словник, що містить відповідність дії та її текстового опису
         const lexicon = {
-          active: 'восстановлено',
-          completed: 'завершено',
-          deleted: 'удалено'
+          active: 'активні',
+          completed: 'завершені',
+          deleted: 'видалені',
+          all:'всі'
         };
         const elTodoDate = elemItem.querySelector('.todo__date');
-        // Генерируем HTML с описанием действия и текущей датой и временем
+        // Генеруємо HTML з описом дії та поточною датою та часом
         const html = `<span>${lexicon[action]}: ${new Date().toLocaleString().slice(0, -3)}</span>`;
-        // Вставляем сгенерированный HTML в элемент с классом "todo__date"
+        // Вставляємо згенерований HTML елемент із класом "todo__date"
         elTodoDate.insertAdjacentHTML('beforeend', html);
       }
-      
-      // Сохраняем состояние списка
-      this.save();
-    } else if (target.classList.contains('todo__add')) {
-      // Если целевой элемент является кнопкой "Добавить", вызываем метод добавления задачи
-      this.add();
-      // Сохраняем состояние списка
-      this.save();
-    }
-  },
+  // Зберігаємо стан списку
+  this.save();
+} else if (target.classList.contains('todo__add')) {
+  // Якщо цільовий елемент є кнопкою "Додати", викликаємо метод додавання задачі
+  this.add();
+  // Зберігаємо стан списку
+  this.save();
+}
+},
 
-  // Добавление задачи
-  add() {
-    const elemText = document.querySelector('.todo__text');
-    const elemDueInput = document.querySelector('.todo__due-input');
-    // Проверяем доступность поля ввода и наличие текста и срока выполнения
-    if (elemText.disabled || !elemText.value.length || !elemDueInput.value){
-      // Если поле ввода заблокировано или пустое, показываем сообщение
-      alert('Введите текст задачи!');
-      return;
-    }
-    // Добавляем новый элемент задачи в список
-    document.querySelector('.todo__items').insertAdjacentHTML('beforeend', this.create(elemText.value, elemDueInput.value));
-    elemText.value = '';
-    elemDueInput.value = '';
-  },
-
-  // Генерация HTML для создания элемента задачи
-  create(text, dueDate) {
-    const date = JSON.stringify({ add: new Date().toLocaleString().slice(0, -3) });
-    return `<li class="todo__item" data-todo-state="active">
-      <span class="todo__task">
-        ${text}
-        <span class="todo__date" data-todo-date="${date}">
-          <span>добавлено: ${new Date().toLocaleString().slice(0, -3)}</span        </span>
-          </span>
-          <span class="todo__due">
-            <span>срок: ${dueDate}</span>
-          </span>
-          <span class="todo__action todo__action_restore" data-todo-action="active"></span>
-          <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
-          <span class="todo__action todo__action_delete" data-todo-action="deleted"></span>
-        </li>`;
-      },
-      
-      // Инициализация списка задач
+// Додавання завдання
+add() {
+  const elemText = document.querySelector('.todo__text');
+  const elemDueInput = document.querySelector('.todo__due-input');
+  // Перевіряємо доступність поля введення та наявність тексту та терміну виконання
+  if (elemText.disabled || !elemText.value.length || !elemDueInput.value){
+    // Якщо поле введення заблоковане або порожнє, показуємо повідомлення
+    alert('Введіть текст завдання!');
+    return;
+  }
+  // Додаємо новий елемент завдання до списку
+  document.querySelector('.todo__items').insertAdjacentHTML('beforeend', this.create(elemText.value, elemDueInput.value));
+  elemText.value = '';
+  elemDueInput.value = '';
+},
+// Генерація HTML до створення елемента завдання
+create(text, dueDate) {
+  const date = JSON.stringify({ add: new Date().toLocaleString().slice(0, -3) });
+  return `<li class="todo__item" data-todo-state="active">
+    <span class="todo__task">
+      ${text}
+      <span class="todo__date" data-todo-date="${date}">
+        <span>додано: ${new Date().toLocaleString().slice(0, -3)}</span </span>
+        </span>
+        <span class="todo__due">
+          <span>термін: ${dueDate}</span>
+        </span>
+        <span class="todo__action todo__action_restore" data-todo-action="active"></span>
+        <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
+        <span class="todo__action todo__action_delete" data-todo-action="deleted"></span>
+      </li>`;
+    },
+      // Ініціалізація списку завдань
       init() {
         const fromStorage = localStorage.getItem('todo');
-        // Если в локальном хранилище есть сохраненные данные, восстанавливаем список задач
+        // Якщо локальному сховищі є збережені дані, відновлюємо список завдань
         if (fromStorage) {
           document.querySelector('.todo__items').innerHTML = fromStorage;
         }
-        // Добавляем обработчик события изменения выбранного варианта в списке фильтров
+        // Додаємо обробник події зміни вибраного варіанта у списку фільтрів
         document.querySelector('.todo__options').addEventListener('change', this.update);
-        // Добавляем обработчик события клика на страницу
+        // Додаємо обробник події кліка на сторінку
         document.addEventListener('click', this.action.bind(this));
       },
       
-      // Обновление состояния списка задач в соответствии с выбранным фильтром
-      update() {
-        const option = document.querySelector('.todo__options').value;
-        // Обновляем атрибут "data-todo-option" у списка задач с выбранным фильтром
-        document.querySelector('.todo__items').dataset.todoOption = option;
-        // Отключаем поле ввода, если выбран фильтр, отличный от "активные"
-        document.querySelector('.todo__text').disabled = option !== 'active';
-      },
-      
-      // Сохранение состояния списка задач в локальном хранилище
+    // Оновлення стану списку завдань відповідно до вибраного фільтра
+    update() {
+      const option = document.querySelector('.todo__options').value;
+      // Оновлюємо атрибут "data-todo-option" у списку завдань із вибраним фільтром
+      document.querySelector('.todo__items').dataset.todoOption = option;
+      // Відключаємо поле введення, якщо вибрано фільтр, відмінний від "активні"
+      document.querySelector('.todo__text').disabled = option !== 'active';
+    },
+// Збереження стану списку завдань у локальному сховищі
       save() {
         localStorage.setItem('todo', document.querySelector('.todo__items').innerHTML);
       }
     };
-    
-    // Инициализация списка задач при загрузке страницы
+    // Ініціалізація списку завдань під час завантаження сторінки
     todo.init();
-    
