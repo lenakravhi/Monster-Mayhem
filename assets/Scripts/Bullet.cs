33da69076adec9e39874465d09ef088f043f9fce
lenakravhi <101953369+lenakravhi@users.noreply.github.com>
2023-06-13 11:56:50 +0300

    Bullet.cs

--- assets/Scripts/Bullet.cs
+++ assets/Scripts/Bullet.cs
@@ -1,42 +1,42 @@
-﻿using UnityEngine;
-using System.Collections;
+using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    // Посилання на батьківський об'єкт (призначається ззовні)
     private GameObject parent;
-    public GameObject Parent { set { parent = value; }  get { return parent; } }
+    public GameObject Parent { set { parent = value; } get { return parent; } }
 
-    private float speed = 10.0F;
+    // Швидкість руху кулі
+    private float speed = 10.0f;
+
+    // Напрямок руху кулі (призначається ззовні)
     private Vector3 direction;
     public Vector3 Direction { set { direction = value; } }
 
+    // Встановлення кольору кулі
     public Color Color
     {
-        set { sprite.color = value; }
-    }
-
-    private SpriteRenderer sprite;
-
-    private void Awake()
-    {
-        sprite = GetComponentInChildren<SpriteRenderer>();
+        set { GetComponentInChildren<SpriteRenderer>().color = value; }
     }
 
     private void Start()
     {
-        Destroy(gameObject, 1.4F);
+        // Знищення кулі через 1.4 секунди після створення
+        Destroy(gameObject, 1.4f);
     }
-	
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        // Переміщення кулі у напрямку direction зі швидкістю speed
+        transform.position += direction * speed * Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
+        // Отримання посилання на компонент Unit об'єкта, з яким зіткнулась куля
         Unit unit = collider.GetComponent<Unit>();
 
+        // Якщо це юніт і він не є батьківським об'єктом кулі, знищуємо кулю
         if (unit && unit.gameObject != parent)
         {
             Destroy(gameObject);
